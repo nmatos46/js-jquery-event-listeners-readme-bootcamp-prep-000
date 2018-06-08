29f4579c30b55d85baa9d079072dd48f8e1ab374
Norman Matos <nmatos419@hotmail.com>
2018-06-08 06:02:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,20 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!');
+  });
+}
+
+function frameIt(){
+  $('.tasty').on('load', function(){
+    $(this).css('border','1px solid red');
+  })
+}
 
 $(document).ready(function(){
 
 // call functions here
+getIt()
+
 
 });
